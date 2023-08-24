791d8a73a69b38c85587baed81fd9e56010560d7
bhargavi <bhargavi@edtex.in>
2023-08-24 14:46:19 +0530

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,8 @@ import { HousingLocation } from '../housing-location';
     </form>
   </section>
   <section class="results">
-    <app-housing-location  [housingLocation]="housingLocation"></app-housing-location>
+    <app-housing-location 
+    *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
   </section>`,
   styleUrls: ['./home.component.css']
 })
@@ -19,15 +20,101 @@ export class HomeComponent {
 
   readonly baseUrl = '/assets/images' ;
 
-  housingLocation : HousingLocation ={
-    id : 9999,
-    name:"Test Home",
-    city:"Test City",
-    state:"ST",
-    photo: `${this.baseUrl}/home1.jpg` ,
-    availableUnits: 99 ,
-    wifi:true,
-    laundry : false,
-  };
+  // housingLocation : HousingLocation ={
+  //   id : 9999,
+  //   name:"Test Home",
+  //   city:"Test City",
+  //   state:"ST",
+  //   photo: `${this.baseUrl}/home1.jpg` ,
+  //   availableUnits: 99 ,
+  //   wifi:true,
+  //   laundry : false,
+  // };
+
+  housingLocationList : HousingLocation[] = [
+    {
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home1.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },
+    {
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home2.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home3.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home4.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home5.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home6.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home7.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home8.jpg` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },{
+      id : 9999,
+        name:"Test Home",
+        city:"Test City",
+        state:"ST",
+        photo: `${this.baseUrl}/home9.webp` ,
+        availableUnits: 99 ,
+        wifi:true,
+        laundry : false,
+    },
+  ]
 
 }
