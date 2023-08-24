fc9a1aafd9af082042e6ad1461d6c9edab6e9638
bhargavi <bhargavi@edtex.in>
2023-08-24 11:12:58 +0530

    Homecomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template :`
+    <main>
+      <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true" height="50">
+      <section class="content">
+        <app-home></app-home>
+      </section>
+
+      </header>
+    </main>`,
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
