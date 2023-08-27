bb52947aff5bd1b0a371746fd0a3cc54c285d7b9
bhargavi <bhargavi@edtex.in>
2023-08-27 23:08:06 +0530

    search + HTTP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,13 +13,13 @@ import { CommonModule } from '@angular/common';
   ],
   template:`<section>
     <form>
-      <input type="text" placeholder="Filter by City">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by City" #filter>
+      <button class="primary" type="button"  (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
     <app-housing-location 
-    *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
   </section>`,
   styleUrls: ['./home.component.css']
 })
@@ -41,8 +41,26 @@ export class HomeComponent {
   housingLocationList : HousingLocation[] = []       
   housingService : HousingService = inject(HousingService)
 
+  filteredLocationList : HousingLocation[] =[];
+
   constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    // this.housingLocationList = this.housingService.getAllHousingLocations();
+    // this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    )
+
   }
 
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -6,103 +6,114 @@ import { HousingLocation } from './housing-location';
 })
 export class HousingService {
 
+  url = 'http://localhost:3000/locations';
+
   constructor() { }
 
-  readonly baseUrl = '/assets/images' ;
-  housingLocationList : HousingLocation[] = [
-    {
-      id : 1,
-        name:"Test Home1",
-        city:"Test City1",
-        state:"ST1",
-        photo: `${this.baseUrl}/home1.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },
-    {
-      id : 2,
-        name:"Test Home2",
-        city:"Test City2",
-        state:"ST2",
-        photo: `${this.baseUrl}/home2.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 3,
-        name:"Test Home3",
-        city:"Test City3",
-        state:"ST3",
-        photo: `${this.baseUrl}/home3.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 4,
-        name:"Test Home4",
-        city:"Test City4",
-        state:"ST4",
-        photo: `${this.baseUrl}/home4.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 5,
-        name:"Test Home5",
-        city:"Test City5",
-        state:"ST5",
-        photo: `${this.baseUrl}/home5.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 6,
-        name:"Test Home6",
-        city:"Test City6",
-        state:"ST",
-        photo: `${this.baseUrl}/home6.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 7,
-        name:"Test Home7",
-        city:"Test City7",
-        state:"ST7",
-        photo: `${this.baseUrl}/home7.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 8,
-        name:"Test Home8",
-        city:"Test City8",
-        state:"ST8",
-        photo: `${this.baseUrl}/home8.jpg` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },{
-      id : 9,
-        name:"Test Home9",
-        city:"Test City9",
-        state:"ST9",
-        photo: `${this.baseUrl}/home9.webp` ,
-        availableUnits: 99 ,
-        wifi:true,
-        laundry : false,
-    },
-  ]
+  // readonly baseUrl = '/assets/images' ;
+  // housingLocationList : HousingLocation[] = [
+  //   {
+  //     id : 1,
+  //       name:"Test Home1",
+  //       city:"Hyderabad",
+  //       state:"ST1",
+  //       photo: `${this.baseUrl}/home1.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },
+  //   {
+  //     id : 2,
+  //       name:"Test Home2",
+  //       city:"Bangalore",
+  //       state:"ST2",
+  //       photo: `${this.baseUrl}/home2.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 3,
+  //       name:"Test Home3",
+  //       city:"Hyderabad",
+  //       state:"ST3",
+  //       photo: `${this.baseUrl}/home3.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 4,
+  //       name:"Test Home4",
+  //       city:"Bangalore",
+  //       state:"ST4",
+  //       photo: `${this.baseUrl}/home4.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 5,
+  //       name:"Test Home5",
+  //       city:"Kolkata",
+  //       state:"ST5",
+  //       photo: `${this.baseUrl}/home5.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 6,
+  //       name:"Test Home6",
+  //       city:"Hyderabad",
+  //       state:"ST",
+  //       photo: `${this.baseUrl}/home6.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 7,
+  //       name:"Test Home7",
+  //       city:"Bangalore",
+  //       state:"ST7",
+  //       photo: `${this.baseUrl}/home7.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 8,
+  //       name:"Test Home8",
+  //       city:"Hyderabad",
+  //       state:"ST8",
+  //       photo: `${this.baseUrl}/home8.jpg` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },{
+  //     id : 9,
+  //       name:"Test Home9",
+  //       city:"Kolkata",
+  //       state:"ST9",
+  //       photo: `${this.baseUrl}/home9.webp` ,
+  //       availableUnits: 99 ,
+  //       wifi:true,
+  //       laundry : false,
+  //   },
+  // ]
 
 
 
-  getAllHousingLocations(): HousingLocation [] {
-      return this.housingLocationList;
+  // getAllHousingLocations(): HousingLocation [] {
+  //     return this.housingLocationList;
+  // }
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById( id: number) :HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id == id);
+  // getHousingLocationById( id: number) :HousingLocation | undefined {
+  //   return this.housingLocationList.find(housingLocation => housingLocation.id == id);
+  // }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName:string, lastName: string , email:string){
